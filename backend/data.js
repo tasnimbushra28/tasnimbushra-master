25bb3857c15c917ccda868d2da334f4cdf51c47d
Raisa <syeda.raisa.afsar@g.bracu.ac.bd>
2022-08-23 18:30:35 +0600

    first commit

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: 'Chicken Burger',
+      slug: 'chicken-burger',
+      category: 'Fastfood',
+      image: '/images/p1.jpg', //679px x 829px
+      price: 150,
+      countInstock: 15,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'hygenic and tasty',
+    },
+    {
+      name: 'Chicken Shwarma',
+      slug: 'chicken-shwarma',
+      category: 'Fastfood',
+      image: '/images/p2.jpg', //679px x 829px
+      price: 150,
+      countInstock: 15,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'hygenic and tasty',
+    },
+    {
+      name: 'Pizza',
+      slug: 'pizza',
+      category: 'Fastfood',
+      image: '/images/p3.jpg', //679px x 829px
+      price: 150,
+      countInstock: 15,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'hygenic and tasty',
+    },
+  ],
+};
+export default data;
